926679a0ccfa8c343af8e23440bf15a54541bebe
dram218 <ruisun@whu.edu.cn>
2016-09-27 17:19:06 +0800

    update

---
+++ CMakeFiles/SEventDetector.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SEventDetector.dir/SEventDetector.o"
+  "SEventDetector.pdb"
+  "SEventDetector"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SEventDetector.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
